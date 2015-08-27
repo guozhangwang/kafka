5794daf330de715e8272fa7e5ecffb6e98b3a46e
Guozhang Wang <wangguoz@gmail.com>
2015-08-26 17:06:18 -0700

    Fix fetch / commit / clean logic

--- stream/src/main/java/org/apache/kafka/streaming/processor/internals/ProcessorContextImpl.java
+++ stream/src/main/java/org/apache/kafka/streaming/processor/internals/ProcessorContextImpl.java
@@ -218,7 +218,7 @@ public class ProcessorContextImpl implements ProcessorContext {
 
     @Override
     public void commit() {
-        task.commit();
+        task.needCommit();
     }
 
     @Override
